d296451560bf76546c43eb7933f6e0708df90d22
Muhamadnursidik <ghoyatulabdan992@gmail.com>
2025-10-03 12:52:55 +0700

    Belajar-Authentication

--- laravel-xirpl3/app/Http/Controllers/PostController.php
+++ laravel-xirpl3/app/Http/Controllers/PostController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 //import model Post
 use App\Models\Post;
-
+//import package Request
+use Illuminate\Http\Request;
 class PostController extends Controller
 {
+    // beri middleware auth untuk megecek sudah login atau belum
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     // daftar post
     public function index() 
     {
@@ -15,4 +22,44 @@ class PostController extends Controller
         return view ('post.index', compact('post'));
     }
     
+    public function create() 
+    {
+        return view('post.create');
+    }
+
+    public function store(Request $request) 
+    {
+        //validasi data
+        $post   = new Post;
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save(); //simpan data ke database
+        return redirect()->route('post.index'); //alihkan halaman ke halaman post (daftar post
+    }
+
+    public function edit($id)
+    {
+        //mencari data post berdasarkan parameter 'id'
+        $post = Post::findOrfail($id);
+        return view('post.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        //mencari data post berdasarkan parameter 'id'
+        $post = Post::findOrfail($id);
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
+        // di alihkan ke halaman post melalui route 'post.index' 
+        return redirect()->route('post.index');
+    }
+
+    public function destroy($id)
+    {
+        //mencari data post berdasarkan parameter 'id'
+        $post = Post::findOrfail($id);
+        $post->delete(); //setelah data di temukan kemudian di hapus
+        return redirect()->route('post.index');
+    }
 }
---
+++ laravel-xirpl3/resources/views/post/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+             <fieldset>
+        <legend>Tambah Data Post</legend>
+        <form action="{{ route('post.store') }}" method="post">
+            @csrf
+            <div class="mb-3">
+                <label for="">Title</label>
+                <input type="text" name="title" class ="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea name="content" class="form-control" required></textarea>
+            </div>
+            <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        </form>
+    </fieldset>
+</div>
+        </div>
+    </div>
