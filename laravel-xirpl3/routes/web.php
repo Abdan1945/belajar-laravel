54f953480690d83e0d929626cf76f7350ba64042
Muhamadnursidik <ghoyatulabdan992@gmail.com>
2025-10-27 14:29:31 +0700

    Tambahan onetomany

--- laravel-xirpl3/routes/web.php
+++ laravel-xirpl3/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\BiodataController;
 use App\Http\Controllers\Mycontroller; //contoller harus di import / di panggil
 use App\Http\Controllers\postController;
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -165,3 +167,8 @@ Route::get('eloquent', [RelasiController::class, 'eloquent']);
 Route::resource('dosen', App\Http\Controllers\DosenController::class)->middleware('auth');
 
 Route::resource('hobi', App\Http\Controllers\HobiController::class)->middleware('auth');
+
+
+Route::resource('mahasiswa', App\Http\Controllers\MahasiswaController::class);
+
+Route::resource('wali', App\Http\Controllers\WaliController::class);
